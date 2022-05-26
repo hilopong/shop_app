10925f9544f3a427a75b9cac898057768311e3bc
김윤성 <hilopong@hotmail.com>
2022-05-27 07:35:51 +0900

    상품리스트 get 으로 가져와서 적용해보기 -  잘 안되네...시부럴2

--- lib/screens/home/components/body.dart
+++ lib/screens/home/components/body.dart
@@ -1,15 +1,58 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 
+import '../../../models/Product.dart';
 import '../../../size_config.dart';
 import 'categories.dart';
 import 'discount_banner.dart';
 import 'home_header.dart';
 import 'popular_product.dart';
 import 'special_offers.dart';
+import 'package:http/http.dart' as http;
 
 class Body extends StatelessWidget {
+  List<Product> prdlist=[];
+
+
+
+  Future getprdlist(int catecode) async {
+    var result = await http.get(Uri.parse(
+        'https://webapi.superdesk.co.kr/AdminProduct/GetPrdList/?companyidx=' +
+            catecode.toString()));
+    var result2 = jsonDecode(result.body);
+
+    for (var i = 0; i < result2.length; i++) {
+      print(result2[i]['NVC_prdName']);
+
+      Product prd = Product(
+        id: result2[i]['I_idx'],
+        images: [
+          "assets/images/ps4_console_white_1.png",
+          "assets/images/ps4_console_white_2.png",
+          "assets/images/ps4_console_white_3.png",
+          "assets/images/ps4_console_white_4.png",
+        ],
+        colors: [
+          Color(0xFFF6625E),
+          Color(0xFF836DB8),
+          Color(0xFFDECB9C),
+          Colors.white,
+        ],
+        title: result2[i]['NVC_prdName'],
+        price: 45.4,
+        description: "test",
+      );
+
+      prdlist.add(prd);
+    }
+
+    print(prdlist[0].title);
+  }
+
   @override
   Widget build(BuildContext context) {
+    getprdlist(36);
     return SafeArea(
       child: SingleChildScrollView(
         child: Column(
@@ -21,7 +64,7 @@ class Body extends StatelessWidget {
             Categories(),
             SpecialOffers(),
             SizedBox(height: getProportionateScreenWidth(30)),
-            PopularProducts(),
+            PopularProducts(prdlist: prdlist),
             SizedBox(height: getProportionateScreenWidth(30)),
           ],
         ),
--- lib/screens/home/components/popular_product.dart
+++ lib/screens/home/components/popular_product.dart
@@ -1,57 +1,18 @@
-import 'dart:convert';
-
 import 'package:flutter/material.dart';
 import 'package:shop_app/components/product_card.dart';
 import 'package:shop_app/models/Product.dart';
 
 import '../../../size_config.dart';
 import 'section_title.dart';
-import 'package:http/http.dart' as http;
 
 class PopularProducts extends StatelessWidget {
-  List<Product> prdlist = [];
+  final List<Product> prdlist;
 
-  Future getprdlist(int catecode) async {
-    var result = await http.get(Uri.parse(
-        'https://webapi.superdesk.co.kr/AdminProduct/GetPrdList/?companyidx=' +
-            catecode.toString()));
-    var result2 = jsonDecode(result.body);
-
-    for (var i = 0; i < result2.length; i++) {
-      print(result2[i]['NVC_prdName']);
-
-      Product prd = Product(
-        id: result2[i]['I_idx'],
-        images: [
-          "assets/images/ps4_console_white_1.png",
-          "assets/images/ps4_console_white_2.png",
-          "assets/images/ps4_console_white_3.png",
-          "assets/images/ps4_console_white_4.png",
-        ],
-        colors: [
-          Color(0xFFF6625E),
-          Color(0xFF836DB8),
-          Color(0xFFDECB9C),
-          Colors.white,
-        ],
-        title: result2[i]['NVC_prdName'],
-        price: 45.4,
-        description: "test",
-      );
-
-      prdlist.add(prd);
-    }
-
-    print(prdlist[0].title);
-  }
+  const PopularProducts({Key? key, required this.prdlist}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return StatefulWrapper(
-      onInit: () {
-        getprdlist(36);
-      },
-      child: Column(
+    return Column(
         children: [
           Padding(
             padding: EdgeInsets.symmetric(
@@ -76,35 +37,26 @@ class PopularProducts extends StatelessWidget {
                 SizedBox(width: getProportionateScreenWidth(20)),
               ],
             ),
+          ),
+          //SizedBox(height: getProportionateScreenWidth(20)),
+          // Padding(
+          //   padding: EdgeInsets.symmetric(
+          //       horizontal: getProportionateScreenWidth(20)),
+          //   child: SectionTitle(title: "Remote Products", press: () {}),
+          // ),
+          ListView.builder(
+              itemCount: prdlist.length,
+              itemBuilder: (context,index){
+                return SizedBox(
+                  child: Text(
+                    prdlist[index].title,
+                    style: TextStyle(color: Colors.black),
+                    maxLines: 2,
+                  ),
+                );
+              }
           )
         ],
-      ),
     );
-  }
-}
-
-/// 상태 위젯에서 onInit 호출을 제공하는 상태 저장 기능 용 래퍼
-class StatefulWrapper extends StatefulWidget {
-  final Function onInit;
-  final Widget child;
-
-  const StatefulWrapper({required this.onInit, required this.child});
-
-  @override
-  _StatefulWrapperState createState() => _StatefulWrapperState();
-}
-
-class _StatefulWrapperState extends State<StatefulWrapper> {
-  @override
-  void initState() {
-    if (widget.onInit != null) {
-      widget.onInit();
-    }
-    super.initState();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return widget.child;
   }
 }
